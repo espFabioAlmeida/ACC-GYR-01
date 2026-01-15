5b3cd77e7fb17a337518510e5a4f9c160f51180b
Fábio Almeida <teg.fabio.almeida@gamil.com>
2026-01-15 14:31:34 -0300

    V1: Preparação do pacote CAN

--- Core/Src/utilities/UtilityCAN.c
+++ Core/Src/utilities/UtilityCAN.c
@@ -23,13 +23,28 @@ void recebePacoteCAN() {
 ENVIA PACOTE CAN
 ==============================================================================*/
 void enviaPacoteCAN() {
+	uint8_t dado = 0;
 	canTxHeader.ExtId = ACC_GYRO_ADDRESS + flagEndereco1;
 	canTxHeader.RTR = CAN_RTR_DATA;
 	canTxHeader.IDE = CAN_ID_EXT;
 	canTxHeader.DLC = 8;
 	canTxHeader.TransmitGlobalTime = DISABLE;
 
-	//TODO: PREPARAR O BUFFER
+	dado = bitWrite(dado, 0, mpu6050Data.accXNegativo);
+	dado = bitWrite(dado, 1, mpu6050Data.accYNegativo);
+	dado = bitWrite(dado, 2, mpu6050Data.accZNegativo);
+	dado = bitWrite(dado, 3, mpu6050Data.gyroXNegativo);
+	dado = bitWrite(dado, 4, mpu6050Data.gyroYNegativo);
+	dado = bitWrite(dado, 5, mpu6050Data.gyroZNegativo);
+
+	canTxBuffer[0] = mpu6050Data.accX;
+	canTxBuffer[1] = mpu6050Data.accY;
+	canTxBuffer[2] = mpu6050Data.accZ;
+	canTxBuffer[3] = mpu6050Data.gyroX;
+	canTxBuffer[4] = mpu6050Data.gyroY;
+	canTxBuffer[5] = mpu6050Data.gyroZ;
+	canTxBuffer[6] = dado;
+	canTxBuffer[7] = 0x00;
 
 	if(HAL_CAN_AddTxMessage(&hcan, &canTxHeader, canTxBuffer, &canTxMailbox) != HAL_OK) {
 	    Error_Handler();
